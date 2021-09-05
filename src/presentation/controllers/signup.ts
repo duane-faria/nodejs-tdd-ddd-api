9c9f2cfab828a2f998869eaebe5de8fab27e6742
duane-faria <duane.faria@etec.sp.gov.br>
2021-09-04 22:25:36 -0300

    feat: return 400 if a given email is invalid

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,8 +2,16 @@ import { HttpRequest, HttpResponse } from '../protocols/http'
 import { Controller } from '../protocols/controller'
 import { MissingParamError } from '../errors/missing-param-errors'
 import { badRequest } from '../helpers/http-helper'
+import { EmailValidator } from '../protocols/email-validator'
+import { InvalidParamError } from '../errors/invalid-param-error'
 
 export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields: string[] = [
       'name',
@@ -16,6 +24,11 @@ export class SignUpController implements Controller {
       if (!httpRequest.body[requiredField]) {
         return badRequest(new MissingParamError(requiredField))
       }
+    }
+
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'))
     }
 
     return {
