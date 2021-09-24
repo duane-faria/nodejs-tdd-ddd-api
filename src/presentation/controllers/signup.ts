eac00663b7e3d30eedabf788c2439a269c2111d7
duane-faria <duane.faria@etec.sp.gov.br>
2021-09-23 21:37:57 -0300

    feat: create server error logic

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,6 +4,7 @@ import { MissingParamError } from '../errors/missing-param-errors'
 import { badRequest } from '../helpers/http-helper'
 import { EmailValidator } from '../protocols/email-validator'
 import { InvalidParamError } from '../errors/invalid-param-error'
+import { ServerError } from '../errors/server-error'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
@@ -13,27 +14,34 @@ export class SignUpController implements Controller {
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields: string[] = [
-      'name',
-      'email',
-      'password',
-      'passwordConfirmation'
-    ]
+    try {
+      const requiredFields: string[] = [
+        'name',
+        'email',
+        'password',
+        'passwordConfirmation'
+      ]
 
-    for (const requiredField of requiredFields) {
-      if (!httpRequest.body[requiredField]) {
-        return badRequest(new MissingParamError(requiredField))
+      for (const requiredField of requiredFields) {
+        if (!httpRequest.body[requiredField]) {
+          return badRequest(new MissingParamError(requiredField))
+        }
       }
-    }
 
-    const isValid = this.emailValidator.isValid(httpRequest.body.email)
-    if (!isValid) {
-      return badRequest(new InvalidParamError('email'))
-    }
+      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
 
-    return {
-      statusCode: 200,
-      body: {}
+      return {
+        statusCode: 200,
+        body: {}
+      }
+    } catch (e) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      }
     }
   }
 }
